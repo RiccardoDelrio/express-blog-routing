5260ba71b41f6441ea3bb7a0323c8eb01325ae61
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-03-13 14:22:07 +0100

    initialize express server and project structure with .gitignore

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+//Server in ascolto
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+
+})
+app.use(express.static('public'));
